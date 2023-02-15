09bf637050aa700815f34c5bfeaba15f648cb2d2
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-02-15 11:42:05 -0300

    refatorando funcao

--- src/Controllers/MotorcycleController.ts
+++ src/Controllers/MotorcycleController.ts
@@ -16,15 +16,7 @@ class MotorcycleController {
   }
 
   public async create() {
-    const motorcycle: IMotorcycle = {
-      model: this.req.body.model,
-      year: this.req.body.year,
-      color: this.req.body.color,
-      status: this.req.body.status,
-      buyValue: this.req.body.buyValue,
-      category: this.req.body.category,
-      engineCapacity: this.req.body.engineCapacity,
-    };
+    const motorcycle: IMotorcycle = this.req.body;
 
     try {
       const newMotorcycle = await this.service.create(motorcycle);
